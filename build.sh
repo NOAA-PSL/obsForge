829cffc421ba831ce1d092b7169c9d4d1634c33c
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2024-11-15 18:24:36 +0000

    ready to try building

---
+++ build.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# build.sh
+# 1 - determine host, load modules on supported hosts; proceed w/o otherwise
+# 2 - configure; build; install
+# 4 - optional, run unit tests
+
+set -eu
+
+dir_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source $dir_root/ush/detect_machine.sh
+
+# ==============================================================================
+usage() {
+  set +x
+  echo
+  echo "Usage: $0 -p <prefix> | -t <target> -h"
+  echo
+  echo "  -p  installation prefix <prefix>    DEFAULT: <none>"
+  echo "  -t  target to build for <target>    DEFAULT: $MACHINE_ID"
+  echo "  -c  additional CMake options        DEFAULT: <none>"
+  echo "  -v  build with verbose output       DEFAULT: NO"
+  echo "  -f  force a clean build             DEFAULT: NO"
+  echo "  -h  display this message and quit"
+  echo
+  exit 1
+}
+
+# ==============================================================================
+
+# Defaults:
+INSTALL_PREFIX=""
+CMAKE_OPTS=""
+BUILD_TARGET="${MACHINE_ID:-'localhost'}"
+BUILD_VERBOSE="NO"
+CLEAN_BUILD="NO"
+COMPILER="${COMPILER:-intel}"
+
+while getopts "p:t:c:hvdfa" opt; do
+  case $opt in
+    p)
+      INSTALL_PREFIX=$OPTARG
+      ;;
+    t)
+      BUILD_TARGET=$OPTARG
+      ;;
+    c)
+      CMAKE_OPTS=$OPTARG
+      ;;
+    v)
+      BUILD_VERBOSE=YES
+      ;;
+    f)
+      CLEAN_BUILD=YES
+      ;;
+    h|\?|:)
+      usage
+      ;;
+  esac
+done
+
+case ${BUILD_TARGET} in
+  hera | orion | hercules | wcoss2 | noaacloud | gaeac5 | gaeac6 )
+    echo "Building obsForge on $BUILD_TARGET"
+    source $dir_root/ush/module-setup.sh
+    module use $dir_root/modulefiles
+    module load obsforge/$BUILD_TARGET.$COMPILER
+    CMAKE_OPTS+=" -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXEC -DMPIEXEC_NUMPROC_FLAG=$MPIEXEC_NPROC -DBUILD_GSIBEC=ON"
+    module list
+    ;;
+  $(hostname))
+    echo "Building obsForge on $BUILD_TARGET"
+    ;;
+  *)
+    echo "Building obsForge on unknown target: $BUILD_TARGET"
+    ;;
+esac
+
+CMAKE_OPTS+=" -DMACHINE=$BUILD_TARGET"
+
+BUILD_DIR=${BUILD_DIR:-$dir_root/build}
+if [[ $CLEAN_BUILD == 'YES' ]]; then
+  [[ -d ${BUILD_DIR} ]] && rm -rf ${BUILD_DIR}
+fi
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR}
+
+# If INSTALL_PREFIX is not empty; install at INSTALL_PREFIX
+[[ -n "${INSTALL_PREFIX:-}" ]] && CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
+
+# Configure
+echo "Configuring ..."
+set -x
+cmake \
+  ${CMAKE_OPTS:-} \
+  $dir_root/bundle
+set +x
+
+# Build
+echo "Building ..."
+set -x
+make -j ${BUILD_JOBS:-6} VERBOSE=$BUILD_VERBOSE
+set +x
+
+# Install
+if [[ -n ${INSTALL_PREFIX:-} ]]; then
+  echo "Installing ..."
+  set -x
+  make install
+  set +x
+fi
+
+exit 0
